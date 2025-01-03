4fafa2b69a139aa6090a38ed4d76c7a50899b0b3
maneesh16m <maneesh16m@gmail.com>
2025-01-03 11:20:11 +1100

    added new files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Products.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Products() {
+  return (
+    <div className="page">
+      <h1>Our Products</h1>
+      <p>Discover our range of freshly baked pastries, cakes, and breads.</p>
+    </div>
+  );
+}
+
+export default Products;
